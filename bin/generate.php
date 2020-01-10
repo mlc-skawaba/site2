1696dc4bcc58bbf1321f9fd5639d1c8099ba982b
mlc-skawaba <kawabata-shohei@mitsue.co.jp>
2020-01-10 15:57:35 +0900

    no message

--- bin/generate.php
+++ bin/generate.php
@@ -2,9 +2,11 @@
 
 require_once './vendor/autoload.php';
 
+define('__APPROOT__', dirname(__DIR__));
+
 $parser = new \cebe\markdown\GithubMarkdown();
 
-$tplDir = realpath(dirname(__DIR__).'/src/templates/');
+$tplDir = realpath(__APPROOT__.'/src/templates/');
 $tpl = new PhpRenderer($tplDir, [ 'markdown' => $parser ]);
 
 $data = file_get_contents('https://xshn7hqya2.execute-api.ap-northeast-1.amazonaws.com/Prod/recipes');
@@ -15,7 +17,7 @@ foreach($data['data'] as $item) {
         if  ($item['lang'] === 'ja' ) {
             $filename = $item['recipe_code'] . '.html';
             $content = $tpl->fetch('/page/recipe.phtml', $item);
-            $r = file_put_contents('./htdocs/' . $filename, $content, LOCK_EX);
+            $r = file_put_contents(__APPROOT__.'/dist/' . $filename, $content, LOCK_EX);
         }
     }
 }
